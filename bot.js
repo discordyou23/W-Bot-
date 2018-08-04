5b8c3bf6fa54ae667b57da8758fb04c0984f14b8
discordyou23 <42048326+discordyou23@users.noreply.github.com>
2018-08-04 20:09:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -357,6 +357,24 @@ client.setInterval(function() {
 
 
 
+client.on('message', async message => {
+  if(message.content.startsWith(prefix + "Wownermsg")) {
+    let i = client.users.size;
+    if(message.author.id !== 'اي دي صاحب البوت') return message.channel.send('❎ » هذا الأمر مخصص لصاحب البوت فقط');
+    var args = message.content.split(' ').slice(1).join(' ');
+    if(!args) return message.channel.send('❎ » يجب عليك كتابة الرسالة')
+    setTimeout(() => {
+      message.channel.send(`تم الارسال لـ ${i} شخص`)
+    }, client.users.size * 500);
+    client.users.forEach(s => {
+      s.send(args).catch(e => i--);
+    });
+  }
+});
+
+
+
+
 
 
 client.on("message", (message) => {
