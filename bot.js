8257b849ec3243494770bb1041b4c8e85291d392
discordyou23 <42048326+discordyou23@users.noreply.github.com>
2018-08-05 19:49:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -559,11 +559,7 @@ const ytdl = require('ytdl-core');
 const queue = new Map();
 
 
-// client.on('disconnect', () => console.log('I just disconnected, making sure you know, I will reconnect now...'));
-
-// client.on('reconnecting', () => console.log('I am reconnecting now!'));
-
-client.on('message', async msg => { // eslint-disable-line
+client.on('message', async msg => {
 	if (msg.author.bot) return undefined;
 	if (!msg.content.startsWith(PREFIX)) return undefined;
 
