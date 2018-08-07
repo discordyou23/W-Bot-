d0194eb219fee29d632b2d0c364232f40c5918ce
discordyou23 <42048326+discordyou23@users.noreply.github.com>
2018-08-07 04:10:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -922,6 +922,33 @@ Server owner: __${guild.owner}__`)
 
 
 
+            
+client.on('message', msg => { 
+    if (msg.content.startsWith(`Wwarn`)) {
+      if(!msg.member.hasPermission("MANAGE_MESSAGES")) return;
+       let args = msg.content.split(" ").slice(1);
+      if (!msg.mentions.members.first()) return msg.reply('منشن الشخص المحدد')
+      if (!args[0]) return msg.reply('اكتب السبب')
+      //غير اسم الروم او سوي روم بذا الاسم 
+      if (msg.guild.channels.find('name', 'warns')) {
+        //اذا غيرت فوق غير هنا كمان 
+        msg.guild.channels.find('name', 'warns').send(`
+      تم اعطائك انذار : ${msg.mentions.members.first()}
+      لأنك قمت بما يلي
+      ${args.join(" ").split(msg.mentions.members.first()).slice(' ')}
+      `)
+      }
+    }
+})
+
+
+
+
+
+
+
+
+
 
 
 
