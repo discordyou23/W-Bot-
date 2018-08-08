c22c64c409eb4a81d2752f0a21ecb2528890ebda
discordyou23 <42048326+discordyou23@users.noreply.github.com>
2018-08-08 22:47:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -357,7 +357,7 @@ client.on('message' , message => {
 const embed = new Discord.RichEmbed()
 .setColor("BLUE")
 .setAuthor(message.author.username, message.author.avatarURL)
-.setTitle('اضغط هنا رابط البوت')
+.setTitle('اضغط على علامة الصح                     |✔')
 .setURL('https://discordapp.com/oauth2/authorize?client_id=474287612388048916&scope=bot&permissions=2146958583')
 message.channel.sendEmbed(embed);
 }
