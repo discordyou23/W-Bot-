745f528bece2322e63bab1c98cba490691bf6a6f
discordyou23 <42048326+discordyou23@users.noreply.github.com>
2018-08-09 21:59:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1342,4 +1342,39 @@ client.on('message', message => {
 
 
 
+
+
+  client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('>bcall')){
+ if (message.author.id !== '') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+ if(!message.author.id === '') return;
+message.channel.sendMessage('جار ارسال الرسالة |✅')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
